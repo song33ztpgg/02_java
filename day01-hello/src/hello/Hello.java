4d4f1be3974aebf5cca6390df99dd907302f1924
song33ztpgg <sasasa7737@egmail.com>
2019-07-04 12:54:08 +0900

    Hello.class

--- day01-hello/src/hello/Hello.java
+++ day01-hello/src/hello/Hello.java
@@ -5,7 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
+System.out.println("hello");
 
+System.out.println("하하");
 	}
 
 }
@@ -13,10 +15,18 @@ System.out.println("hello");
 
 
 
+// day01-hello \ (패키지) \
 
 
 /*
 대소문자에 엄격하다
-실행키 : F11 +
+실행키 : F11 + 
+
+
+git add =  git add to index 
+commit  =   
+ > 표시는 저장이 되지 않았다는 것 
+ sysout -> ctrl + space bar 
+ 
 */
  
