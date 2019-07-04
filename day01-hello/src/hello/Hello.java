1e0516e7fa69eca5f6de1410c7067a5e4bf07abe
song33ztpgg <sasasa7737@egmail.com>
2019-07-04 10:34:09 +0900

    day01-hello/

---
+++ day01-hello/src/hello/Hello.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+
+	}
+
+}
+
+
+
+
+
+
+/*
+대소문자에 엄격하다
+실행키 : F11 +
+*/
+ 
