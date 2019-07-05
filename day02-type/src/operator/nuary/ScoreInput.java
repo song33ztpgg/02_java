fa2da58b712b530d0cf25a01648b8cdb48e49b99
song33ztpgg <sasasa7737@egmail.com>
2019-07-05 15:50:26 +0900

    저장

--- day02-type/src/operator/nuary/ScoreInput.java
+++ day02-type/src/operator/nuary/ScoreInput.java
@@ -1,10 +1,23 @@
 package operator.nuary;
 
+import java.util.Scanner;
+
+// && || 조건문
 public class ScoreInput {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		Scanner scan;
+		//스캐너 초기화
+		scan = new Scanner(System.in); 
+		int scan1;
+		// 변수에 스캐너로 값을 입력하여 초기화 
+		scan1 = scan.nextInt(); 
+	
+		
+		
+		
 	}
 
 }
