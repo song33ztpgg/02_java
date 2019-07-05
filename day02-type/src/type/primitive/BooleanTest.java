55fecec07e36802b4e51509881ca3c61afcb24d7
song33ztpgg <sasasa7737@egmail.com>
2019-07-05 10:03:57 +0900

    day02-type/

---
+++ day02-type/src/type/primitive/BooleanTest.java
@@ -0,0 +1,33 @@
+package type.primitive;
+/*
+ 
+ 논리 타입
+ 
+ */
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+		//1.선언 
+		
+		boolean isLike; 
+		boolean isExists; 
+		boolean isHave; 
+		boolean isEnd = false; // 선언과 동시에 초기화 
+		
+		//2.초기화
+		isLike = true; 
+		isExists = true; 
+		isHave = false;
+
+		
+		//3.사용 
+		
+		System.out.println("isLike  = " +isLike);
+		System.out.println("isExists = " +isExists);
+		System.out.println("isHave  = " +isHave);
+		
+		
+	}
+
+}
