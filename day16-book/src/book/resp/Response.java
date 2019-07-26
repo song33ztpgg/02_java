6f0ab6645177000c222f663ace76cde0954355d4
song33ztpgg <sasasa7737@egmail.com>
2019-07-26 15:56:05 +0900

    --
    ---
    
    ---

--- day16-book/src/book/resp/Response.java
+++ day16-book/src/book/resp/Response.java
@@ -1,5 +1,16 @@
 package book.resp;
-
+/**
+ * 북매니저가 어떤 작업에 대해 
+ * 응답하는 방식을 정의하는 인터페이스 
+ * 
+ * 
+ * */
 public interface Response {
 
+	/**
+	 * 매지저가 특정작업에 대해 
+	 * 응답하는 메소드
+	 * */
+	public abstract void response(Object object);
+	 
 }
