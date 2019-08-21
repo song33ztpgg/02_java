1e4eddc5520cf2b4acfeabbc11d408f86588fd86
song33ztpgg <sasasa7737@egmail.com>
2019-08-21 19:46:51 +0900

    재귀함수 사용

--- lab09-송종호.ArithProgression/src/sequence/ArithProgression.java
+++ lab09-송종호.ArithProgression/src/sequence/ArithProgression.java
@@ -1,54 +1,23 @@
 package sequence;
 
 public class ArithProgression {
-
-//	private int start; 
-//	private int diff; 
-//	private int index; 
-	 int result = 0;
-//	
+	
+	//맴버변수 =
+	private int result = 0;
+	
+	//계산을 하는 매소드 
 	public int sequence(int start, int diff, int index) { 
 		
-		if(index != 0) {
+		//index 값이 0이 아닐 때 까지 실행
+		if (index != 0) {
+			//계산
 			result += start + diff * (index - 1);
+			//호출을 할때마다 idx 값 감소
+			index--; 
+			//제귀 호출
 			sequence(start, diff, index);
 		}
 		
-		index--;
-		
 		return result;
 	}
-	
-//	public void setStart(int start) { 
-//		this.start = start;
-//	}
-//	
-//	public void setDiff(int diff) { 
-//		this.start = start;
-//	}
-//	
-//	public void setIndex(int index) { 
-//		this.start = start;
-//	}
-	
 }
-
-
-//---------------------------------------------
-//
-//첫항 : start
-//공차 : diff
-//항의 갯수 : index 
-//--------------------------------------------------------
-//첫 항이 start 이고, 공차가 diff 이며, 항의 갯수 index 만큼인
-//등차수열의 합을 구하는 메소드 sequence 를 작성하시오.
-//
-//등차 수열의 일반항은
-//start + diff * (index - 1) 로 구한다.
-//--------------------------------------------------------
-//+ArithProgression     : class 
-//+sequence(int start, int diff, int index) : int
-//--------------------------------------------------------
-//+ArithProgressionTest : test class
-//+main()
-//start : 1, diff : 3, index : 3 ==> 12
--- lab09-송종호.ArithProgression/src/sequence/ArithProgressionTest.java
+++ lab09-송종호.ArithProgression/src/sequence/ArithProgressionTest.java
@@ -3,16 +3,15 @@ package sequence;
 public class ArithProgressionTest {
 
 	public static void main(String[] args) {
+		
+		//클래스 호출 , 초기화
 		ArithProgression arithProgression = new ArithProgression(); 
 		
-		System.out.println("첫항 1");
-	
-		System.out.println("공차 2"); 
-	 
-		System.out.println("항의 개수 5");
+		System.out.printf("첫항 1 / ");
+		System.out.printf("공차 3 / "); 
+		System.out.println("항의 개수 3");
 		
-		
-		System.out.println(arithProgression.sequence(1,2,5));
+		//결과값 출력 
+		System.out.println(arithProgression.sequence(1,3,3));
 	}
-
 }
