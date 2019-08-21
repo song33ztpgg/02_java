aa233198da7b17d94098edea151c3d756836e1da
song33ztpgg <sasasa7737@egmail.com>
2019-08-21 15:57:11 +0900

    --

--- lab09-송종호.ArithProgression/src/sequence/ArithProgressionTest.java
+++ lab09-송종호.ArithProgression/src/sequence/ArithProgressionTest.java
@@ -3,7 +3,14 @@ package sequence;
 public class ArithProgressionTest {
 
 	public static void main(String[] args) {
-
+		ArithProgression arithProgression = new ArithProgression(); 
+		
+		System.out.println("첫항 1");
+		arithProgression.setStart(1);
+		System.out.println("공차 2"); 
+		arithProgression.setDiff();  
+		System.out.println("항의 개수 5");
+		arithProgression.setIndex(); 
 	}
 
 }
